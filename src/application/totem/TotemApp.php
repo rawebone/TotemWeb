e10a12b8f931b5636e6ae61c1d7b1b2f2752932d
Nick Rawe <nickrawe2@gmail.com>
2012-02-10 21:26:45 +0000

    Build system and first write of the index file

---
+++ src/application/totem/TotemApp.php
@@ -0,0 +1,32 @@
+<?php
+namespace Totem\Application;
+
+use Respect\Rest\Router;
+
+class TotemApp
+{
+    protected $request;
+    
+    public function run()
+    {
+        //  Create the router
+        $this->request = new Router();
+        
+        //  Setup routes
+        $this->setupRoutes();
+        
+        //  Dispatch the request
+        $this->request->dispatch();
+    }
+    
+    public function setupRoutes()
+    {
+        $r = $this->request;
+        
+        $r->get("/", function () {
+            echo "Hello";
+        });
+    }
+}
+
+?>
